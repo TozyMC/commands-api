b6c8461204b951473928f9ff60b71fbdd7128192
TozyMC <tozymc@gmail.com>
2020-12-06 10:46:50 +0700

    Rename Utils.java to Reflections.java & add wrapper knownCommands field

--- src/main/java/xyz/tozymc/spigot/api/command/util/Reflections.java
+++ src/main/java/xyz/tozymc/spigot/api/command/util/Reflections.java
@@ -2,6 +2,7 @@ package xyz.tozymc.spigot.api.command.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
+import org.bukkit.command.Command;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.Plugin;
@@ -11,31 +12,48 @@ import org.jetbrains.annotations.Nullable;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Map;
 
-public final class Utils {
+/**
+ * @author TozyMC
+ * @since 2.0
+ */
+public final class Reflections {
 
   private static final MethodHandle PLUGIN_COMMAND_CONSTRUCTOR;
   private static final MethodHandle COMMAND_MAP_FIELD;
+  private static final MethodHandle KNOWN_COMMANDS_FIELD;
 
   static {
     MethodHandles.Lookup lookup = MethodHandles.lookup();
     MethodHandle plCmdConstructor = null;
     MethodHandle commandMapField = null;
+    MethodHandle knownCommandsField = null;
     try {
-      MethodType plCmdMethodType = MethodType.methodType(void.class, String.class, Plugin.class);
-      plCmdConstructor = lookup.findConstructor(PluginCommand.class, plCmdMethodType);
+      Constructor<PluginCommand> reflectConstructor = PluginCommand.class
+          .getDeclaredConstructor(String.class, Plugin.class);
+      reflectConstructor.setAccessible(true);
+      plCmdConstructor = lookup.unreflectConstructor(reflectConstructor);
 
       Class<?> serverClass = Bukkit.getServer().getClass();
-      commandMapField = lookup.findGetter(serverClass, "commandMap", SimpleCommandMap.class);
+      Field commandMapReflect = serverClass.getDeclaredField("commandMap");
+      commandMapReflect.setAccessible(true);
+      commandMapField = lookup.unreflectGetter(commandMapReflect);
+
+      Field knownCommandsReflect = SimpleCommandMap.class.getDeclaredField("knownCommands");
+      knownCommandsReflect.setAccessible(true);
+      knownCommandsField = lookup.unreflectGetter(knownCommandsReflect);
     } catch (Exception ignored) {
     }
 
     PLUGIN_COMMAND_CONSTRUCTOR = plCmdConstructor;
     COMMAND_MAP_FIELD = commandMapField;
+    KNOWN_COMMANDS_FIELD = knownCommandsField;
   }
 
-  private Utils() {
+  private Reflections() {
   }
 
   /**
@@ -60,11 +78,25 @@ public final class Utils {
    * @param server The bukkit server
    * @return The command map
    */
-  @Nullable
   @Contract(pure = true)
   public static SimpleCommandMap getCommandMap(Server server) {
     try {
       return (SimpleCommandMap) COMMAND_MAP_FIELD.invoke(server);
+    } catch (Throwable throwable) {
+      return null;
+    }
+  }
+
+  /**
+   * Gets {@code knownCommands} field in {@link SimpleCommandMap}.
+   *
+   * @param commandMap The SimpleCommandMap instance
+   * @return The knownCommands in SimpleCommandMap
+   */
+  public static Map<String, Command> getKnownCommands(SimpleCommandMap commandMap) {
+    try {
+      //noinspection unchecked
+      return (Map<String, Command>) KNOWN_COMMANDS_FIELD.invoke(commandMap);
     } catch (Throwable throwable) {
       return null;
     }
